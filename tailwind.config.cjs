6f8e45d08c189d345e44be843e07bc64fa6f7a83
genmetsaveer <gen.metsaveer@gmail.com>
2023-03-12 20:31:31 +0200

    add minimal content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,20 @@ module.exports = {
     "./src/**/*.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#121112',
+        'turquoise': '#525dcb',
+        'light-green': '#53cb82',
+        'white': '#dedede',
+        'salmon': '#FD7B67',
+        'gamboge': '#EFA22E'
+      },
+      boxShadow: {
+        '2xl': '.5rem .5rem 0 0rem',
+        '3xl': '1rem 1rem 0 0rem'
+      }
+    },
   },
   plugins: [],
 }
